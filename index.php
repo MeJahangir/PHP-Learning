b96fe9d1f6d4e3fb3ee596bad71abdbfdd629a7a
unknown <jahangiralam85860@gmail.com>
2023-11-19 21:50:22 +0600

    sub

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
-<?php 
-
-echo "heloo world";
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Class - 1</title>
+</head>
+<body>
+  
+    <?php
+      echo "hello jahangir, welcome to my php files";
+    ?>
+</body>
+</html>
