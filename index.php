7559186bd10f59d7695845dc9c3880e34aab3b93
unknown <jahangiralam85860@gmail.com>
2023-11-19 21:36:11 +0600

    submit

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
-<?php declare(strict_types=1); // strict requirement
-function setHeight(int $minheight = 50) {
-  echo "The height is : $minheight <br>";
-}
+<?php 
 
-setHeight(350);
-setHeight(); // will use the default value of 50
-setHeight(135);
-setHeight(80);
+echo "heloo world";
+
 ?>
