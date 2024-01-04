13024484deb7a261738fae5f5e6eae8f6b5f0a83
unknown <jahangiralam85860@gmail.com>
2024-01-04 09:03:57 +0600

    submit

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 
     <?php
-      echo 'hello bangladesh';
+      $variable1 = 10;
+      $variable2 = 20;
+      $variable3 = 30;
+      
+      $sum = $variable1 + $variable2 + $variable3;
+      
+      echo "The sum is: " . $sum;
     ?>
     
