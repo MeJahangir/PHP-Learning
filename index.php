9ff125c5ccb31f51211c667c9787f6f7a01c7fef
unknown <jahangiralam85860@gmail.com>
2024-01-04 09:16:04 +0600

    push

--- index.php
+++ index.php
@@ -7,5 +7,14 @@
       $sum = $variable1 + $variable2 + $variable3;
       
       echo "The sum is: " . $sum;
+     
+    
+      $variable1 = "fast";
+      $variable2 = "medele";
+      $variable3 = "last";
+
+      $sum = $variable1 . " " . $variable2 . $variable3;
+
+      echo "The sum is: " . $sum;
     ?>
     
