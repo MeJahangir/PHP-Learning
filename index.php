dcf94f14f36a460138751784c0d488f409396e5b
unknown <jahangiralam85860@gmail.com>
2024-01-05 09:42:38 +0600

    submit

--- index.php
+++ index.php
@@ -4,9 +4,8 @@
       $variable2 = 20;
       $variable3 = 30;
       
-      $sum = $variable1 + $variable2 + $variable3;
-      
-      echo "The sum is: " . $sum;
+      echo "{$variable1}, {$variable2}, {$variable3}\n";
+      echo "{$variable1}, {$variable2}, {$variable3}\n";
      
     
       $variable1 = "fast";
