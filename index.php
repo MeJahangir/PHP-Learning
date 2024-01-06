a5411ffe61b0452ebd66e55c26403ade03510a71
unknown <jahangiralam85860@gmail.com>
2024-01-07 00:48:51 +0600

    push

--- index.php
+++ index.php
@@ -1,46 +1,45 @@
 
-    <?php
-      $variable1 = 10;
-      $variable2 = 20;
-      $variable3 = 30;
-      
-      echo "{$variable1}, {$variable2}, {$variable3}\n";
-      echo "{$variable1}, {$variable2}, {$variable3}\n";
-     
-    
-      $variable1 = "fast";
-      $variable2 = "medele";
-      $variable3 = "last";
+<?php
+  $variable1 = 10;
+  $variable2 = 20;
+  $variable3 = 30;
+  
+  echo "{$variable1}, {$variable2}, {$variable3}\n";
+  echo "{$variable1}, {$variable2}, {$variable3}\n";
+  
 
-      $sum = $variable1 . " " . $variable2 . $variable3;
-      echo "The sum is: " . $sum;
+  $variable1 = "fast";
+  $variable2 = "medele";
+  $variable3 = "last";
 
-      $a = 4;
-      while ($a <= 10) {
-        echo "hello bangladesh $a<br>";
-        $a++;
-      }
-      $counter = 1;
+  $sum = $variable1 . " " . $variable2 . $variable3;
+  echo "The sum is: " . $sum;
 
-      while ($counter <= 5) {
-          echo "Count: $counter<br>";
-          $counter++;
-      }
-      for ($i=0; $i <= 100; $i++) { 
-        echo $i;
-      }
+  $a = 4;
+  while ($a <= 10) {
+    echo "hello bangladesh $a<br>";
+    $a++;
+  }
+  $counter = 1;
 
-      $test = array('jahangir', 'alam', 'joy', 'rakib');
-      echo $test[1];
-      
-      $score = 95;
-      if ($score <= 90) {
-          echo "Excellent!";
-      } elseif ($score <= 70) {
-          echo "Good job!";
-      } elseif ($score <= 50) {
-          echo "You passed.";
-      } else {
-          echo "Sorry, you didn't pass.";
-      }
-i
+  while ($counter <= 5) {
+      echo "Count: $counter<br>";
+      $counter++;
+  }
+  for ($i=0; $i <= 100; $i++) { 
+    echo $i;
+  }
+
+  $test = array('jahangir', 'alam', 'joy', 'rakib');
+  echo $test[1];
+  
+  $score = 95;
+  if ($score <= 90) {
+      echo "Excellent!";
+  } elseif ($score <= 70) {
+      echo "Good job!";
+  } elseif ($score <= 50) {
+      echo "You passed.";
+  } else {
+      echo "Sorry, you didn't pass.";
+  } 
