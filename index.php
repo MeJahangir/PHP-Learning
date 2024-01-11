91588719fc19ed4b1e9b909b3314199c775e0038
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-01-11 21:09:13 +0600

    p

--- index.php
+++ index.php
@@ -1,49 +1,65 @@
 
 <?php
-  $variable1 = 10;
-  $variable2 = 20;
-  $variable3 = 30;
+  // $variable1 = 10;
+  // $variable2 = 20;
+  // $variable3 = 30;
   
-  echo "{$variable1}, {$variable2}, {$variable3}\n";
-  echo "{$variable1}, {$variable2}, {$variable3}\n";
+  // echo "{$variable1}, {$variable2}, {$variable3}\n";
+  // echo "{$variable1}, {$variable2}, {$variable3}\n";
   
 
-  $variable1 = "fast";
-  $variable2 = "medele";
-  $variable3 = "last";
+  // $variable1 = "fast";
+  // $variable2 = "medele";
+  // $variable3 = "last";
 
-  $sum = $variable1 . " " . $variable2 . $variable3;
-  echo "The sum is: " . $sum;
+  // $sum = $variable1 . " " . $variable2 . $variable3;
+  // echo "The sum is: " . $sum;
 
-  $a = 4;
-  while ($a <= 10) {
-    echo "hello bangladesh $a<br>";
-    $a++;
-  }
-  $counter = 1;
+  // $a = 4;
+  // while ($a <= 10) {
+  //   echo "hello bangladesh $a<br>";
+  //   $a++;
+  // }
+  // $counter = 1;
 
-  while ($counter <= 5) {
-      echo "Count: $counter<br>";
-      $counter++;
-  }
-  for ($i=0; $i <= 100; $i++) { 
-    echo $i;
-  }
+  // while ($counter <= 5) {
+  //     echo "Count: $counter<br>";
+  //     $counter++;
+  // }
+  // for ($i=0; $i <= 100; $i++) { 
+  //   echo $i;
+  // }
 
-  $test = array('jahangir', 'alam', 'joy', 'rakib');
-  echo $test[1];
+  // $test = array('jahangir', 'alam', 'joy', 'rakib');
+  // echo $test[1];
   
-  $score = 95;
-  if ($score <= 90) {
-      echo "Excellent!";
-  } elseif ($score <= 70) {
-      echo "Good job!";
-  } elseif ($score <= 50) {
-      echo "You passed.";
-  } else {
-      echo "Sorry, you didn't pass.";
-  } 
-  $name = "jahanir alam joy";
-  $age = 45;
-  var_dump($name, $age);
+  // $score = 95;
+  // if ($score <= 90) {
+  //     echo "Excellent!";
+  // } elseif ($score <= 70) {
+  //     echo "Good job!";
+  // } elseif ($score <= 50) {
+  //     echo "You passed.";
+  // } else {
+  //     echo "Sorry, you didn't pass.";
+  // } 
+  // $name = "jahanir alam joy";
+  // $age = 45;
+  // var_dump($name, $age);
 
+
+//   $url = 'https://www.example.com/somefile.txt';
+//   $content = file_get_contents($url);
+  
+//   if ($content !== false) {
+//       // Do something with the contents of the file
+//       echo $content;
+//   } else {
+//       // Handle error
+//echo "Failed to retrieve file contents.";
+//   }
+
+
+// readfile('text')
+echo "hello bangladesh";
+   
