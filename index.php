af859beb66942d756ab9dbdb39c1347bd6cd27ae
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-02-05 23:59:07 +0600

    push

--- index.php
+++ index.php
@@ -188,3 +188,17 @@ foreach ($colors as $x) {
   echo "$x <br>";
 }
 
+class Car {
+  public $color;
+  public $model;
+  public function __construct($color, $model) {
+    $this->color = $color;
+    $this->model = $model;
+  }
+}
+
+$myCar = new Car("red", "Volvo");
+
+foreach ($myCar as $x => $y) {
+  echo "$x: $y<br>";
+}
