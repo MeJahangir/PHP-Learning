12922e456069ec1c6231e127d0f4300be945b1f9
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-02-10 09:02:15 +0600

    P

--- index.php
+++ index.php
@@ -63,250 +63,250 @@
 // readfile('text')
 
 // Global scope 
-$x = 5;
-function myTest(){
-  echo "<p>variable x inside function is:</p>";
-}
-myTest();
-echo "<p>variable x outside function is: $x</p>";
+// $x = 5;
+// function myTest(){
+//   echo "<p>variable x inside function is:</p>";
+// }
+// myTest();
+// echo "<p>variable x outside function is: $x</p>";
 
 // local scope
-function myText() {
-  $x = 5;
-  echo "Hello Bangladesh $x";
-}
-myText();
-echo "Hello bangladesh is: $x";
+// function myText() {
+//   $x = 5;
+//   echo "Hello Bangladesh $x";
+// }
+// myText();
+// echo "Hello bangladesh is: $x";
 
 // printf diye onek gulo variable print system
-$fName = 'jahangir';
-$lName = 'alam';
-printf("my %s is %s %s","name" ,$fName, $lName);
+// $fName = 'jahangir';
+// $lName = 'alam';
+// printf("my %s is %s %s","name" ,$fName, $lName);
 // result : my name is jahangir alam;
 
 // length check
-$lengthCheck = "Hello World";
-echo strlen($lengthCheck);
-echo '<br>';
-echo str_word_count($lengthCheck);
+// $lengthCheck = "Hello World";
+// echo strlen($lengthCheck);
+// echo '<br>';
+// echo str_word_count($lengthCheck);
 
 // variable ar kisu word cut korty
-$x = "Hello World";
-echo substr($x, 2, 6);
+// $x = "Hello World";
+// echo substr($x, 2, 6);
 
 // php constant declear
-const testing = "jahangir alam joy";
-echo testing;
+// const testing = "jahangir alam joy";
+// echo testing;
 
-define("cars", [
-  "alfa romeo",
-  "BMW",
-  "Toyota"
-]);
-echo cars[2];
+// define("cars", [
+//   "alfa romeo",
+//   "BMW",
+//   "Toyota"
+// ]);
+// echo cars[2];
 
-echo '<br>';
+// echo '<br>';
 // php conditon add 
-$jahangir = 100;
-if ($jahangir > 110) {
-  echo "Have a good day";
-}else{
-  echo "Hello Bangladesh";
-}
+// $jahangir = 100;
+// if ($jahangir > 110) {
+//   echo "Have a good day";
+// }else{
+//   echo "Hello Bangladesh";
+// }
 
-echo '<br>';
-if ($jahangir == 100) {
-  echo "Hello World";
-}
+// echo '<br>';
+// if ($jahangir == 100) {
+//   echo "Hello World";
+// }
 
-echo '<br>';
-$a = 13;
-if ($a > 10) {
-  echo "Above 10";
-  if ($a > 20) {
-    echo " and also above 20";
-  } else {
-    echo " but not above 20";
-  }
-}
+// echo '<br>';
+// $a = 13;
+// if ($a > 10) {
+//   echo "Above 10";
+//   if ($a > 20) {
+//     echo " and also above 20";
+//   } else {
+//     echo " but not above 20";
+//   }
+// }
 
-echo '<br>';
-$favcolor = "green";
-switch ($favcolor) {
-  case "red":
-    echo "Your favorite color is red!";
-    break;
-  case "blue":
-    "Your favorite color is blue!";
-    break;
-  case "green":
-    echo "Your favorite color is green!";
-    break;
-  default:
-    echo "Your favorite color is neither red, blue, nor green!";
-}
+// echo '<br>';
+// $favcolor = "green";
+// switch ($favcolor) {
+//   case "red":
+//     echo "Your favorite color is red!";
+//     break;
+//   case "blue":
+//     "Your favorite color is blue!";
+//     break;
+//   case "green":
+//     echo "Your favorite color is green!";
+//     break;
+//   default:
+//     echo "Your favorite color is neither red, blue, nor green!";
+// }
 
-echo '<br>';
-$d = 4;
-switch ($d) {
-  case 6:
-    echo "Today is Saturday";
-    break;
-  case 0:
-    echo "Today is Sunday";
-    break;
-  default:
-    echo "Looking forward to the Weekend";
-}
+// echo '<br>';
+// $d = 4;
+// switch ($d) {
+//   case 6:
+//     echo "Today is Saturday";
+//     break;
+//   case 0:
+//     echo "Today is Sunday";
+//     break;
+//   default:
+//     echo "Looking forward to the Weekend";
+// }
 
-echo '<br>';
+// echo '<br>';
 // php loops
 // while loops 
-$i = 6;
-while ($i < 10) {
-  echo $i;
-  $i++;
-}
+// $i = 6;
+// while ($i < 10) {
+//   echo $i;
+//   $i++;
+// }
 
 // do while loop
-$i = 1;
+// $i = 1;
 
-do {
-  echo $i;
-  $i++;
-} while ($i < 6);
+// do {
+//   echo $i;
+//   $i++;
+// } while ($i < 6);
 
 // for loops
-for ($x = 0; $x <= 10; $x++) {
-  echo "The number is: $x <br>";
-}
+// for ($x = 0; $x <= 10; $x++) {
+//   echo "The number is: $x <br>";
+// }
 
 // foreach area
-$nemes = array("jahangir","monirul","joy","nur");
+// $nemes = array("jahangir","monirul","joy","nur");
 
-foreach($nemes as $x):
-echo "$x<br>";
-endforeach;
+// foreach($nemes as $x):
+// echo "$x<br>";
+// endforeach;
 
 // braak area add
-$x = 0;
+// $x = 0;
  
-while($x < 10) {
-  if ($x == 4) {
-    break;
-  }
-  echo "The number is: $x <br>";
-  $x++;
-}
+// while($x < 10) {
+//   if ($x == 4) {
+//     break;
+//   }
+//   echo "The number is: $x <br>";
+//   $x++;
+// }
 
 // continue area add
-$x = 0;
+// $x = 0;
  
-while($x < 10) {
-  $x++;
-  if ($x == 4) {
-    continue;
-  }
-  echo "The number is: $x <br>";
-} 
+// while($x < 10) {
+//   $x++;
+//   if ($x == 4) {
+//     continue;
+//   }
+//   echo "The number is: $x <br>";
+// } 
 
-$color = array("red", "green", "blue", "yellow");
+// $color = array("red", "green", "blue", "yellow");
 
-foreach ($color as $x) {
-  if ($x == "green") continue;
-    echo "$x <br>";
+// foreach ($color as $x) {
+//   if ($x == "green") continue;
+//     echo "$x <br>";
   
-}
+// }
 
-function FunctionName() {
-  echo "Jahangir alam joy";
-}
+// function FunctionName() {
+//   echo "Jahangir alam joy";
+// }
 
-FunctionName();
+// FunctionName();
 
-function familyName($fName) {
-  echo "$fName islam.<br>";
-}
-familyName("jani");
-familyName("jahangir");
-familyName("nur");
-familyName("hamid");
-familyName("joy");
+// function familyName($fName) {
+//   echo "$fName islam.<br>";
+// }
+// familyName("jani");
+// familyName("jahangir");
+// familyName("nur");
+// familyName("hamid");
+// familyName("joy");
 
-echo "<br>";
+// echo "<br>";
 
-$n = 12;
-$o = 020;
-printf("The number is %d and %d", $n, $o);
-echo "<br>";
-printf("Octal number %o = %d", 0200000, 0200000);
+// $n = 12;
+// $o = 020;
+// printf("The number is %d and %d", $n, $o);
+// echo "<br>";
+// printf("Octal number %o = %d", 0200000, 0200000);
 
-echo "<br>";
+// echo "<br>";
 // absulet function
-echo (abs(7.9));
-echo "<br>";
+// echo (abs(7.9));
+// echo "<br>";
 // global function
-$m = 100;
-function testFunction() {
-  echo "How old are you?";
-}
-testFunction();
+// $m = 100;
+// function testFunction() {
+//   echo "How old are you?";
+// }
+// testFunction();
 
-echo '<br>';
+// echo '<br>';
 // local variable
-function jahangir() {
-  $m = 10;
-  echo "Give me a $m taka please"; 
-}
-jahangir();
+// function jahangir() {
+//   $m = 10;
+//   echo "Give me a $m taka please"; 
+// }
+// jahangir();
 
-echo '<br>';
+// echo '<br>';
 
 // php the global keyword
-$x = 100;
-$y = 90;
-function globalFun() {
-  global $x, $y;
-  $y = $x + $y;
-}
-globalFun();
-echo $y;
+// $x = 100;
+// $y = 90;
+// function globalFun() {
+//   global $x, $y;
+//   $y = $x + $y;
+// }
+// globalFun();
+// echo $y;
 
-echo '<br>';
+// echo '<br>';
 
-$info = '1235678';
-var_dump($info);
+// $info = '1235678';
+// var_dump($info);
 
-echo '<br>';
+// echo '<br>';
 
 // array value function and foreach loop kore ber korlam
-$cars = array('volvo', 'BMW', 'Toyota');
-function carsName() {
-  global $cars;
-  foreach ($cars as $car) {
-    echo $car . "<br>";
-  }
-}
-carsName();
+// $cars = array('volvo', 'BMW', 'Toyota');
+// function carsName() {
+//   global $cars;
+//   foreach ($cars as $car) {
+//     echo $car . "<br>";
+//   }
+// }
+// carsName();
 
-echo "<br>";
+// echo "<br>";
 
 // Php object add 
-class Car{
-  public $make;
-  public $model;
-  public $year;
+// class Car{
+//   public $make;
+//   public $model;
+//   public $year;
 
-  public function __construct($make, $model, $year) {
-    $this->make = $make;
-    $this->model = $model;
-    $this->year = $year;
-  }
+//   public function __construct($make, $model, $year) {
+//     $this->make = $make;
+//     $this->model = $model;
+//     $this->year = $year;
+//   }
 
-  public function carInfo() {
-    return "Make" . $this->make . " " . "<br>" . " Model" . $this->model . " " . "<br>" . "Year"  . $this->year;
-  }
-}
+//   public function carInfo() {
+//     return "Make" . $this->make . " " . "<br>" . " Model" . $this->model . " " . "<br>" . "Year"  . $this->year;
+//   }
+// }
 
-$carResult = new Car("toyota", "camry", "2020");
-echo $carResult->carInfo();
+// $carResult = new Car("toyota", "camry", "2020");
+// echo $carResult->carInfo();
