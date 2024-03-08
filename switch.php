009c72541a51d1521418c1b4802c5a4f6558a9eb
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-03-08 20:09:33 +0600

    submit switch case

---
+++ switch.php
@@ -0,0 +1,19 @@
+<?php
+// switch function add 
+// switch cach use kore amra kono kisu check korty pari
+$age = 100;
+
+switch($age){
+    case 10:
+        echo 'you are still a child';
+        break;
+    case 20:
+        echo 'You are not a child anymore';
+        break;
+    case 100:
+        echo 'You are out of luck now';
+        break;
+    default:
+        echo 'You are not yet born';
+        break;
+};
