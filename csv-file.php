4ce179e5e62b91d00c955e651f68e0faf6dee3f6
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-03-11 00:03:43 +0600

    submit data input csv file

--- csv-file.php
+++ csv-file.php
@@ -3,26 +3,49 @@
 if (isset($_POST['submit'])) {
     $fName = $_POST['FName'];
     $lName = $_POST['LName'];
+    $email = $_POST['Email'];
+
+    //click if data folder add 
+    if (!file_exists('data')) {
+        mkdir('data', 0777, true);
+    }
+
+    // Check if photo folder exists
+    if (!file_exists('photo')) {
+        mkdir('photo', 0777, true);
+    }
+    // photo area add 
+    $image = $_FILES['Photo'];
+    
+    $photos = 'photo/' . str_replace(' ', '+', $imagename);
+    //The path you wish to upload the image to
+    $imagePath = "images/";
+
+    if(is_uploaded_file($imagetemp)) {
+        if(move_uploaded_file($imagetemp, $photos)) {
+            // echo "Sussecfully uploaded your image.";
+        }
+        else {
+            echo "Failed to move your image.";
+        }
+    }
+    else {
+        echo "Failed to upload your image.";
+    }
+
+    //click if file add
+    if (!file_exists('data/data.csv')) {
+        $file = fopen('data/data.csv', 'w');//write
+        $inputName = str_replace(' ', '+', $fName); // Replace spaces in first name
+        fputcsv($file, [$fName, $lName, $email]);
+    } else {
+        $file = fopen('data/data.csv', 'a');//append
+        $inputName = str_replace(' ', '+', $fName); // Replace spaces in first name
+        fputcsv($file, [$fName, $lName, $email]);
+    }
+
+    fclose($file); // Close the file handle after writing
 }
-
-// click if data folder add 
-if (!file_exists('data')) {
-    mkdir('data', 0777, true);
-}
-
-//click if file add
-if (!file_exists('data/data.csv')) {
-    $file = fopen('data/data.csv', 'w');//write
-    $inputName = str_replace(' ', '+', $lName); // Replace spaces in last name
-    fputcsv($file, [$fName, $lName]);
-} else {
-    $file = fopen('data/data.csv', 'a');//append
-    $inputName = str_replace(' ', '+', $lName); // Replace spaces in last name
-    fputcsv($file, [$fName, $lName]);
-}
-
-fclose($file); // Close the file handle after writing
-
 
 //data read 
 $file = fopen('data/data.csv', 'r');
@@ -32,19 +55,16 @@ while (($line = fgetcsv($file)) !== FALSE) {
     $testData[] = $line;
 }
 
-foreach ($testData as $valueCheck) {
-    
-}
-
-
-
-
+fclose($file); // Close the file handle after reading
 
 ?>
 
-<div style="width: 200px; height : 200px; border: 1px solid gray; text-align:center; padding-top: 20px; margin:auto;">
-    <input style="margin-top: 10px;" type="text" name="FName" placeholder="First- Name">
-    <input style="margin-top: 10px;" type="text" name="LName" placeholder="Last-Name">
-    <input style="margin-top: 10px;" type="submit" name="submit">
-</div>
-
+<form method="post" enctype="multipart/form-data">
+    <div style="width: 200px; height: 200px; border: 1px solid gray; text-align: center; padding-top: 20px; margin: auto;">
+        <input style="margin-top: 10px;" type="text" name="FName" placeholder="First Name">
+        <input style="margin-top: 10px;" type="text" name="LName" placeholder="Last Name">
+        <input style="margin-top: 10px;" type="email" name="Email" placeholder="johndoe@example.com">
+        <input style="margin-top: 10px; margin-left: 11px; font-size: 10px; " type="file" name="Photo" placeholder="">
+        <input style="margin-top: 10px;" type="submit" name="submit">
+    </div>
+</form>
--- example.php
+++ example.php
@@ -11,29 +11,35 @@ $maximumNumber = [12,10,20,40,44,60,100,103];
 echo min($maximumNumber);
 echo '<br>';
 
-?>
+$name = "jahangir alam joy";
+echo ucfirst($name);
+echo '<br>';
+echo ucwords($name);
+echo '<br>';
+echo lcfirst($name);
+echo '<br>';
+echo strtolower($name);
+echo "<br>";
+
+$post_title = "OUR PHP CLASS NO 8";
+$post_slug = strtolower($post_title);
+$post_slug = str_replace(' ', '-', $post_slug);
+echo $post_slug;
+echo "<br>";
 
 
-<?php
-// Data to be added to the CSV file
-$newData = ["John", "Doe", "john.doe@example.com"];
 
-// Path to the CSV file
-$csvFile = "file/test.cs";
 
-// Open the CSV file in append mode ('a')
-$file = fopen($csvFile, 'a');
 
-// Check if file is successfully opened
-if ($file) {
-    // Write data to the CSV file
-    fputcsv($file, $newData);
 
-    // Close the file
-    fclose($file);
 
-    echo "Data added to CSV file successfully.";
-} else {
-    echo "Failed to open CSV file.";
-}
+
+
+
+
+
+
+
+
+
 ?>
