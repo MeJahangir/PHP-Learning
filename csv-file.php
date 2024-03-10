5927143121e2c0455f166e7ef45d6c71c16b3825
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-03-10 20:44:51 +0600

    submit

--- csv-file.php
+++ csv-file.php
@@ -1,19 +1,50 @@
 <?php
-
-//Data to be added to the csv file
-$newData = ['jahangir', 'alam', 'jahangir@gmail.com'];
-
-//path to the csv file
-$csvFile = 'data.csv';
-
-//Open the csv file in append mode ('a')
-$file = fopen($csvFile, 'a');
-
-// click if file is successfully opened
-if ($file) {
-    fputcsv($file, $newData);
-    fclose($file);
-    echo 'Data added to csv file successfully';
-}else{
-    echo 'Failed to open csv file';
+// csv file data add 
+if (isset($_POST['submit'])) {
+    $fName = $_POST['FName'];
+    $lName = $_POST['LName'];
 }
+
+// click if data folder add 
+if (!file_exists('data')) {
+    mkdir('data', 0777, true);
+}
+
+//click if file add
+if (!file_exists('data/data.csv')) {
+    $file = fopen('data/data.csv', 'w');//write
+    $inputName = str_replace(' ', '+', $lName); // Replace spaces in last name
+    fputcsv($file, [$fName, $lName]);
+} else {
+    $file = fopen('data/data.csv', 'a');//append
+    $inputName = str_replace(' ', '+', $lName); // Replace spaces in last name
+    fputcsv($file, [$fName, $lName]);
+}
+
+fclose($file); // Close the file handle after writing
+
+
+//data read 
+$file = fopen('data/data.csv', 'r');
+$testData = [];
+
+while (($line = fgetcsv($file)) !== FALSE) {
+    $testData[] = $line;
+}
+
+foreach ($testData as $valueCheck) {
+    
+}
+
+
+
+
+
+?>
+
+<div style="width: 200px; height : 200px; border: 1px solid gray; text-align:center; padding-top: 20px; margin:auto;">
+    <input style="margin-top: 10px;" type="text" name="FName" placeholder="First- Name">
+    <input style="margin-top: 10px;" type="text" name="LName" placeholder="Last-Name">
+    <input style="margin-top: 10px;" type="submit" name="submit">
+</div>
+
