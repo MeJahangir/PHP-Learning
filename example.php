bc7ca6029d65cdb3e9bd2ac5804ea529e641ac46
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-03-12 22:21:14 +0600

    submit

--- example.php
+++ example.php
@@ -21,25 +21,32 @@ echo '<br>';
 echo strtolower($name);
 echo "<br>";
 
-$post_title = "OUR PHP CLASS NO 8";
+$post_title = "OUR PHP CLASS NO 8 ";
 $post_slug = strtolower($post_title);
-$post_slug = str_replace(' ', '-', $post_slug);
+$post_slug = strtolower(trim($post_title));
+$post_slug = str_replace(' ', '+', $post_slug);
 echo $post_slug;
 echo "<br>";
 
+$nameSearch = ['jahangir', 'hamid', 'masud'];
+
+if (!in_array('monirul', $nameSearch)) {
+    echo 'Name does not match';
+}
+echo "<br>";
+
+$names = ('jahangir alam joy masud rana hamid sofiq');
+
+if(strstr($names, 'joy')){
+    echo "you are right";
+}else{
+    echo "you are wrong";
+}
+ 
 
 
+$vawel = "MDJAHANGIRALAM";
+var_dump(str_split($vawel));
 
-
-
-
-
-
-
-
-
-
-
-
-
-?>
+ 
+?> 
