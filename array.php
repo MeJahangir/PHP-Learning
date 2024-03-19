f0fa5741af775308a7cf73ee5cd740d561658757
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-03-19 16:57:21 +0600

    push

--- array.php
+++ array.php
@@ -69,13 +69,33 @@ $student = [
 //     echo '<hr>';
 // };
 
+// or 
+
 // Loop through each student and display their data
-for ($i = 0; $i < count($student); $i++) {
-     echo "Student " . ($i + 1) . '<br>'
-    . "Name: " . strtolower($student[$i]["name"]) . '<br>'
-    . "Age: " . $student[$i]["age"] . '<br>'
-    . "Gender: " . $student[$i]["gender"] . '<br>'
-    . "Roll: " . $student[$i]["roll"] . '<br>'
-    .  "Grade: " . $student[$i]["grade"] . '<br>'
-    . '<hr>';
-}
+
+        // for ($i = 0; $i < count($student); $i++) {
+        //     echo "Student " . ($i + 1) . '<br>'
+        // . "Name: " . strtolower($student[$i]["name"]) . '<br>'
+        // . "Age: " . $student[$i]["age"] . '<br>'
+        // . "Gender: " . $student[$i]["gender"] . '<br>'
+        // . "Roll: " . $student[$i]["roll"] . '<br>'
+        // .  "Grade: " . $student[$i]["grade"] . '<br>'
+        // . '<hr>';
+        // }
+?>
+
+    <div style="margin: auto;
+    width: 300px;">
+        <?php for ($i = 0; $i < count($student); $i++) { ?>
+            <div style="margin-left: 20px;">
+                <h3 style="text-align: center; border: 1px solid red;"> <?php echo "Student " . ($i + 1) ?></h3>
+                <div style="border: 1px solid green; padding: 10px">
+                    <p> <?php echo "Name: " . strtoupper($student[$i]["name"]) ?> </p>
+                    <p> <?php echo "Age: " . strtoupper($student[$i]["age"]) ?> </p>
+                    <p> <?php echo "Gender: " . strtoupper($student[$i]["gender"]) ?> </p>
+                    <p> <?php echo "Roll: " . strtoupper($student[$i]["roll"]) ?> </p>
+                    <p> <?php echo "Grade: " . strtoupper($student[$i]["grade"]) ?> </p>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
