bf065120f5fd46983d54f624cf61ebeaf42e064c
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-03-19 15:42:37 +0600

    for loop in array data input

--- array.php
+++ array.php
@@ -61,10 +61,21 @@ $student = [
     ]
 ];
 
-foreach ($student as $studentData) {
-    echo strtoupper("Name : " . $studentData['name'] . '<br>' 
-    . "Age : " . $studentData['age'] . '<br>' 
-    . "Gender " . $studentData['gender'] . '<br>'
-    . "Roll " . $studentData['roll']);
-    echo '<hr>';
-};
+// foreach ($student as $studentData) {
+//     echo strtoupper("Name : " . $studentData['name'] . '<br>' 
+//     . "Age : " . $studentData['age'] . '<br>' 
+//     . "Gender " . $studentData['gender'] . '<br>'
+//     . "Roll " . $studentData['roll']);
+//     echo '<hr>';
+// };
+
+// Loop through each student and display their data
+for ($i = 0; $i < count($student); $i++) {
+     echo "Student " . ($i + 1) . '<br>'
+    . "Name: " . strtolower($student[$i]["name"]) . '<br>'
+    . "Age: " . $student[$i]["age"] . '<br>'
+    . "Gender: " . $student[$i]["gender"] . '<br>'
+    . "Roll: " . $student[$i]["roll"] . '<br>'
+    .  "Grade: " . $student[$i]["grade"] . '<br>'
+    . '<hr>';
+}
