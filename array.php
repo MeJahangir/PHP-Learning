0fe8f18e0a2b9922fe3d9cdbd5cccacc68452b7d
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-03-19 14:11:37 +0600

    array info test

--- array.php
+++ array.php
@@ -1,3 +1,70 @@
 <?php
 
-// Multi daimantional array
+//array test in php forlooping
+
+$student = [
+    [
+    "name" => "MD JAHANGIR ALAM",
+    "age" => 24,
+    "gender" => "Male",
+    "roll" => 20,
+    "grade" => "A"
+    ],
+    [
+    "name" => "MD HAMID PK",
+    "age" => 24,
+    "gender" => "Male",
+    "roll" => 20,
+    "grade" => "A"
+    ],
+    [
+    "name" => "MD MASUD RANA",
+    "age" => 24,
+    "gender" => "Male",
+    "roll" => 20,
+    "grade" => "A"
+    ],
+    [
+    "name" => "MD SHOFIQUL ISLAM",
+    "age" => 24,
+    "gender" => "Male",
+    "roll" => 20,
+    "grade" => "A"
+    ],
+    [
+    "name" => "MD RAJU AHMED",
+    "age" => 24,
+    "gender" => "Male",
+    "roll" => 20,
+    "grade" => "A"
+    ],
+    [
+    "name" => "MD SUJON SARKAR",
+    "age" => 24,
+    "gender" => "Male",
+    "roll" => 20,
+    "grade" => "A"
+    ],
+    [
+    "name" => "MD MONIRUL ISLAM",
+    "age" => 24,
+    "gender" => "Male",
+    "roll" => 20,
+    "grade" => "A"
+    ],
+    [
+    "name" => "MD MANIK RANA",
+    "age" => 24,
+    "gender" => "Male",
+    "roll" => 20,
+    "grade" => "A"
+    ]
+];
+
+foreach ($student as $studentData) {
+    echo strtoupper("Name : " . $studentData['name'] . '<br>' 
+    . "Age : " . $studentData['age'] . '<br>' 
+    . "Gender " . $studentData['gender'] . '<br>'
+    . "Roll " . $studentData['roll']);
+    echo '<hr>';
+};
